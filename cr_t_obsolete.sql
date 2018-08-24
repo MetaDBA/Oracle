1b817f0a24925e252c8add0a01aae0de15ad0216
ewelvang <Administrator@RTPPRORDG001.metabolon.com>
2018-08-24 09:41:12 -0400

    testing code promotion from server to git ...

---
+++ cr_t_obsolete.sql
@@ -0,0 +1,26 @@
+--------------------------------------------------------
+--  File created - Thursday-August-23-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table OBSOLETE
+--------------------------------------------------------
+
+  CREATE TABLE "DBADMIN"."OBSOLETE" 
+   (	"OBJECT_NAME" VARCHAR2(30 BYTE), 
+	"NUM_ROWS" NUMBER, 
+	"REFERENCED_BY" VARCHAR2(30 BYTE), 
+	"LAST_KNOWN_USE" DATE, 
+	"BYTES" NUMBER, 
+	"TABLESPACE" VARCHAR2(30 BYTE), 
+	"DEPENDENCIES" VARCHAR2(30 BYTE), 
+	"CREATED" DATE, 
+	"PARTITIONED" VARCHAR2(1 BYTE), 
+	"DUPLICATE_OF" VARCHAR2(30 BYTE), 
+	"OBJECT_TYPE" VARCHAR2(30 BYTE), 
+	"STATUS" VARCHAR2(8 BYTE)
+   ) SEGMENT CREATION DEFERRED 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  TABLESPACE "USERS" ;
+REM INSERTING into DBADMIN.OBSOLETE
+SET DEFINE OFF;
